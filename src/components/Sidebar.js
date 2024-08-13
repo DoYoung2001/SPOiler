0807c7c546cdc8f88d490feae5968bd59f6b4028
EunBi <dmsgod270@gmail.com>
2024-08-13 15:04:29 +0900

    Playlist Page 구현

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TracklistAlert from "./TracklistAlert"; // 모달 컴포넌트 임포트
 import "../styles/TracklistAlert.css"; // 스타일 파일 임포트
+import { useNavigate } from "react-router-dom"; // useNavigate 훅 임포트
 
 const Sidebar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -28,9 +29,23 @@ const Sidebar = () => {
     setShowConfirmDialog(false);
   };
 
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  // 홈 버튼 클릭 시 MainContent로 이동
+  const handleHomeClick = () => {
+    navigate("/"); // 루트 경로로 이동
+  };
+
+  // 내 플레이리스트 클릭 시 호출될 함수
+  const goToPlaylist = () => {
+    navigate("/playlist");
+  };
+
   return (
     <aside className="sidebar">
-      <div className="sidebar-item">
+      <div className="sidebar-item" onClick={handleHomeClick}>
+        {" "}
+        {/* 홈 클릭 시 handleHomeClick 호출 */}
         <div className="icon">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -42,19 +57,19 @@ const Sidebar = () => {
             <path d="M224,115.55V208a16,16,0,0,1-16,16H168a16,16,0,0,1-16-16V168a8,8,0,0,0-8-8H112a8,8,0,0,0-8,8v40a16,16,0,0,1-16,16H48a16,16,0,0,1-16-16V115.55a16,16,0,0,1,5.17-11.78l80-75.48.11-.11a16,16,0,0,1,21.53,0,1.14,1.14,0,0,0,.11.11l80,75.48A16,16,0,0,1,224,115.55Z" />
           </svg>
         </div>
-        <div className="sidebar-item">
-          <div className="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24px"
-              height="24px"
-              fill="currentColor"
-              viewBox="0 0 256 256"
-            >
-              <path d="M32,64a8,8,0,0,1,8-8H216a8,8,0,0,1,0,16H40A8,8,0,0,1,32,64Zm8,72H160a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16Zm72,48H40a8,8,0,0,0,0,16h72a8,8,0,0,0,0-16Zm135.66-57.7a8,8,0,0,1-10,5.36L208,122.75V192a32.05,32.05,0,1,1-16-27.69V112a8,8,0,0,1,10.3-7.66l40,12A8,8,0,0,1,247.66,126.3ZM192,192a16,16,0,1,0-16,16A16,16,0,0,0,192,192Z" />
-            </svg>
-          </div>
-          <p>내 플레이리스트</p>
+        <p>홈</p>
+      </div>
+      <div className="sidebar-item" onClick={goToPlaylist}>
+        <div className="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24px"
+            height="24px"
+            fill="currentColor"
+            viewBox="0 0 256 256"
+          >
+            <path d="M32,64a8,8,0,0,1,8-8H216a8,8,0,0,1,0,16H40A8,8,0,0,1,32,64Zm8,72H160a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16Zm72,48H40a8,8,0,0,0,0,16h72a8,8,0,0,0,0-16Zm135.66-57.7a8,8,0,0,1-10,5.36L208,122.75V192a32.05,32.05,0,1,1-16-27.69V112a8,8,0,0,1,10.3-7.66l40,12A8,8,0,0,1,247.66,126.3ZM192,192a16,16,0,1,0-16,16A16,16,0,0,0,192,192Z" />
+          </svg>
         </div>
         <p>내 플레이리스트</p>
       </div>

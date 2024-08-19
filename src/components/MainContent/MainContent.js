a8eee80577c4cc49ae38b8b6194405073fb96252
jihye <qnddjqnddj88@naver.com>
2024-08-19 17:18:58 +0900

    현재 날씨 API 불러오고 현재 날씨에 맞는 추천 노래 리스트 추가

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,31 +3,36 @@ import FeaturedPlaylists from "../FeaturedPlaylists/FeaturedPlaylists";
 import styles from "./MainContent.module.css";
 import NewReleases from "../NewReleases/NewReleases";
 import SpotifyGenreTracks from "../Genre/Genre";
+import WeatherBasedRecommendations from "../WeatherBasedRecommendations/WeatherBasedRecommendations";
+import { useWeather } from "../../hooks/useWeather";
 
-const MainContent = () => (
-  <main className={styles["main-content"]}>
-    <div className={styles["content-grid"]}>
-      <div className={styles["content-box"]}>
-        <div>
-          <NewReleases />
+const MainContent = ({ lat, lon }) => {
+  const { data } = useWeather(lat, lon);
+
+  return (
+    <main className={styles["main-content"]}>
+      <div className={styles["content-grid"]}>
+        <div className={styles["content-box"]}>
+          <div>
+            <NewReleases />
+          </div>
+        </div>
+        <div className={styles["content-box"]}>
+          <div>
+            <FeaturedPlaylists />
+          </div>
+        </div>
+        <div className={styles["content-box"]}>
+          <div>
+            <SpotifyGenreTracks />
+          </div>
+        </div>
+        <div className={styles["content-box"]}>
+          {data ? <WeatherBasedRecommendations weather={data.weather[0].main} /> : "날씨 정보를 불러오는 중..."}
         </div>
       </div>
-      <div className={styles["content-box"]}>
-        <div>
-          <FeaturedPlaylists />
-        </div>
-      </div>
-      <div className={styles["content-box"]}>
-        <div>
-          <SpotifyGenreTracks />
-        </div>
-      </div>
-      <div className={styles["content-box"]}>
-        <p className={styles["content-box"] + " " + styles["title"]}>추천 재생목록</p>
-        <div>{/* 추천 재생목록 내용 추가 */}</div>
-      </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 export default MainContent;
